ec2fde4bc15d19478965e6593ac60c5af02131fd
Arash Hatami <hatamiarash7@gmail.com>
2023-05-28 11:41:24 +0330

    cmd: create main

---
+++ cmd/main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"context"
+	_ "net/http/pprof"
+	"os"
+	"os/signal"
+	"sync"
+	"syscall"
+	"time"
+
+	"github.com/arvancloud/uptime-webhook/configs"
+	"github.com/arvancloud/uptime-webhook/internal/platform/application"
+	log "github.com/sirupsen/logrus"
+)
+
+var config *configs.Config
+
+func init() {
+	cfg, err := configs.Load("configs/config.yml")
+	if err != nil {
+		log.WithError(err).Fatal("failed to load configs")
+	}
+	config = cfg
+
+	if err = application.SetupLogger(cfg); err != nil {
+		log.WithError(err).Fatal("failed to setup logger")
+	}
+}
+
+func main() {
+	ctx, cancel := context.WithCancel(context.Background())
+	wg := &sync.WaitGroup{}
+
+	app, err := application.NewApplication(ctx, config)
+	if err != nil {
+		log.WithError(err).Fatal("could not initialize application")
+	}
+
+	app.RunHttpServer(ctx, wg)
+
+	closeSignal := make(chan os.Signal, 1)
+	signal.Notify(closeSignal, syscall.SIGTERM, syscall.SIGINT, os.Interrupt)
+	select {
+	case <-closeSignal:
+		log.Info("Terminating by os signal")
+	case <-ctx.Done():
+		log.Info("Terminating by context cancellation")
+	}
+
+	time.Sleep(time.Duration(1000) * time.Millisecond)
+	cancel()
+	wg.Wait()
+
+	if err = app.Shutdown(); err != nil {
+		log.WithError(err).Panic("application shutdown encountered error")
+	}
+}
