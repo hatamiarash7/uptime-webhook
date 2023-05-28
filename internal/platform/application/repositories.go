8a30fbb8e0d1771b5a665975a876d7402ea9d90d
Arash Hatami <hatamiarash7@gmail.com>
2023-05-28 11:38:19 +0330

    platform: add repositories

---
+++ internal/platform/application/repositories.go
@@ -0,0 +1,13 @@
+package application
+
+import (
+	"github.com/arvancloud/uptime-webhook/internal/platform/repositories/alert"
+)
+
+func (a *App) registerRepositories() {
+	a.registerAlertRepository()
+}
+
+func (a *App) registerAlertRepository() {
+	a.Repositories.AlertRepository = alert.NewAlertRepository(1)
+}
