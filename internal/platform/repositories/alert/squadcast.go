a54b34a38baf6ea33ddae62abc31accbec931b83
Arash Hatami <hatamiarash7@gmail.com>
2023-07-12 13:56:00 +0330

    move alert actions to pool

--- internal/platform/repositories/alert/squadcast.go
+++ internal/platform/repositories/alert/squadcast.go
@@ -3,17 +3,13 @@ package alert
 import (
 	"encoding/json"
 	"strings"
-	"sync"
 
 	"github.com/hatamiarash7/uptime-webhook/internal/models"
-	log "github.com/sirupsen/logrus"
 )
 
 // CreateSquadcastIncident creates an incident in squadcast
 func (r *Repository) CreateSquadcastIncident(alert models.Alert) error {
 	var urls []string
-	var wg sync.WaitGroup
-	results := make(chan string)
 
 	for _, tag := range alert.Data.Service.Tags {
 		urls = append(urls, r.config.Notifier.Squadcast.Teams[strings.ToLower(tag)])
@@ -27,23 +23,8 @@ func (r *Repository) CreateSquadcastIncident(alert models.Alert) error {
 	}
 
 	for _, url := range urls {
-		wg.Add(1)
-		go func(url string) {
-			defer wg.Done()
-			result, err := sendPOSTRequest(url, body)
-			if err != nil {
-				log.WithError(err).Error("[SQUADCAST] Error sending request to " + url)
-				return
-			}
-			log.Debugf("[SQUADCAST] Result: %s", result)
-			results <- result
-		}(url)
+		r.CallSquadcast(url, body)
 	}
-
-	go func() {
-		wg.Wait()
-		close(results)
-	}()
 
 	return nil
 }
--- internal/platform/repositories/alert/telegram.go
+++ internal/platform/repositories/alert/telegram.go
@@ -3,18 +3,13 @@ package alert
 import (
 	"encoding/json"
 	"net/url"
-	netUrl "net/url"
-	"sync"
 
 	"github.com/hatamiarash7/uptime-webhook/internal/models"
-	log "github.com/sirupsen/logrus"
 )
 
 // CreateTelegramMessage sends a telegram message
 func (r *Repository) CreateTelegramMessage(alert models.Alert) error {
 	var urls []string
-	var wg sync.WaitGroup
-	results := make(chan string)
 
 	for _, tag := range alert.Data.Service.Tags {
 		team := r.config.Notifier.Telegram.Teams[tag][0]
@@ -36,47 +31,8 @@ func (r *Repository) CreateTelegramMessage(alert models.Alert) error {
 	}
 
 	for _, url := range urls {
-		wg.Add(1)
-		go func(url string) {
-			defer wg.Done()
-			result, err := sendPOSTRequest(url, body)
-			if err != nil {
-				log.WithError(err).Error("[TELEGRAM] Error sending request to " + url)
-				return
-			}
-
-			log.Debugf("[TELEGRAM] Result: %s", result)
-
-			r := make(map[string]interface{})
-			err = json.Unmarshal([]byte(result), &r)
-			if err != nil {
-				log.WithError(err).Error("[TELEGRAM] Error unmarshalling response")
-			}
-			if price, ok := r["ok"].(bool); ok {
-				if price == false {
-					// Extract the query parameters
-					parsedURL, err := netUrl.Parse(url)
-					if err != nil {
-						log.WithError(err).Error("[TELEGRAM] Error parsing URL")
-					}
-					queryParams := parsedURL.Query()
-
-					log.WithFields(log.Fields{
-						"error_code":  r["error_code"],
-						"description": r["description"],
-						"chat":        queryParams.Get("chat_id"),
-					}).Error("[TELEGRAM] Failed to send message")
-				}
-			}
-
-			results <- result
-		}(url)
+		r.CallTelegram(url, body)
 	}
-
-	go func() {
-		wg.Wait()
-		close(results)
-	}()
 
 	return nil
 }
