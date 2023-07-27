2801eaeed2cc8d448ac53b5cd7228bfc6a4505a1
Arash Hatami <hatamiarash7@gmail.com>
2023-07-27 23:32:36 +0330

    Update slack.go

--- internal/models/slack.go
+++ internal/models/slack.go
@@ -1,6 +1,6 @@
 package models
 
-// TelegramMessage is a struct for Telegram message
+// SlackMessage is a struct for Telegram message
 type SlackMessage struct {
 	Text string `json:"text,omitempty"`
 }
